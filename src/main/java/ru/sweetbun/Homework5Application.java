16d79c48cb722e119e1bd212717cec4d93577e1e
Semyon <s.e.m.e.n.e.m.tancor.disco@gmail.com>
2024-10-07 17:40:39 +0700

    init branch

--- src/main/java/ru/sweetbun/Homework5Application.java
+++ src/main/java/ru/sweetbun/Homework5Application.java
@@ -2,31 +2,11 @@ package ru.sweetbun;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
-import ru.sweetbun.entity.Category;
-import ru.sweetbun.entity.Location;
-import ru.sweetbun.storage.Storage;
 
 @SpringBootApplication
 public class Homework5Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Homework5Application.class, args);
-	}
-
-	@Bean
-	public RestTemplate restTemplate() {
-		return new RestTemplate();
-	}
-
-	@Bean
-	public Storage<Category> categoryStorage() {
-		return new Storage<>();
-	}
-
-	@Bean
-	public Storage<Location> locationStorage() {
-		return new Storage<>();
 	}
 }
