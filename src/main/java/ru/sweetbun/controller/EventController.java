790835785356a588ceda3b6bc94a0f84bf968948
Semyon <s.e.m.e.n.e.m.tancor.disco@gmail.com>
2024-10-15 23:29:17 +0700

    Add point_3

--- src/main/java/ru/sweetbun/controller/EventController.java
+++ src/main/java/ru/sweetbun/controller/EventController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import ru.sweetbun.entity.Event;
 import ru.sweetbun.service.EventService;
+import ru.sweetbun.service.ReactorEventService;
+import reactor.core.publisher.Mono;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -18,9 +20,12 @@ public class EventController {
 
     private final EventService eventService;
 
+    private final ReactorEventService reactorEventService;
+
     @Autowired
-    public EventController(EventService eventService) {
+    public EventController(EventService eventService, ReactorEventService reactorEventService) {
         this.eventService = eventService;
+        this.reactorEventService = reactorEventService;
     }
 
     @GetMapping
@@ -30,5 +35,14 @@ public class EventController {
             @RequestParam(required = false) LocalDate dateFrom,
             @RequestParam(required = false) LocalDate dateTo) {
         return eventService.getAvailableEvents(budget, currency, dateFrom, dateTo);
+    }
+
+    @GetMapping("reactor")
+    public Mono<List<Event>> getEventsReactor(
+            @RequestParam Double budget,
+            @RequestParam String currency,
+            @RequestParam(required = false) LocalDate dateFrom,
+            @RequestParam(required = false) LocalDate dateTo) {
+        return reactorEventService.getAvailableEvents(budget, currency, dateFrom, dateTo);
     }
 }
---
+++ src/main/java/ru/sweetbun/service/ReactorEventService.java
@@ -0,0 +1,81 @@
+package ru.sweetbun.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import reactor.core.publisher.Mono;
+import ru.sweetbun.DTO.EventsResponse;
+import ru.sweetbun.entity.Event;
+
+import java.sql.Date;
+import java.time.LocalDate;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Service
+public class ReactorEventService {
+
+    private final RestTemplate restTemplate;
+
+    private final CurrencyService currencyService;
+
+    private final static String URL_EVENTS = "https://kudago.com/public-api/v1.4/events/";
+
+    @Autowired
+    public ReactorEventService(RestTemplate restTemplate, CurrencyService currencyService) {
+        this.restTemplate = restTemplate;
+        this.currencyService = currencyService;
+    }
+
+    public Mono<List<Event>> getAvailableEvents(Double budget, String currency,
+                                                LocalDate dateFrom, LocalDate dateTo) {
+        if (dateFrom == null) {
+            dateFrom = LocalDate.now();
+        }
+        if (dateTo == null) {
+            dateTo = LocalDate.now().plusDays(7);
+        }
+        String url = URL_EVENTS + "?actual_since=" + (Date.valueOf(dateFrom).getTime() / 1000L)
+                + "&actual_until=" + (Date.valueOf(dateTo).getTime() / 1000L)
+                + "&expand=place"
+                + "&fields=id,title,price,favorites_count,place"
+                + "&page_size=100"
+                + "&order_by=-favorites_count";
+        log.info("URL_EVENT of request: {}", url);
+
+        Mono<List<Event>> eventsMono = Mono.fromCallable(() -> fetchEvents(url, EventsResponse.class));
+
+        Mono<Double> convertedBudgetMono = Mono.fromCallable(() -> {
+            if (currency.equalsIgnoreCase("RUB")) return budget;
+            return currencyService.convertCurrencyToRUB(currency, budget);
+        });
+
+        return Mono.zip(eventsMono, convertedBudgetMono)
+                .flatMap(tuple -> {
+                    List<Event> events = tuple.getT1();
+                    Double convertedBudget = tuple.getT2();
+
+                    List<Event> filteredEvents = events.stream()
+                            .filter(event -> event.getPriceAsDouble() <= convertedBudget)
+                            .collect(Collectors.toList());
+
+                    return Mono.just(filteredEvents);
+                });
+
+    }
+
+    public List<Event> fetchEvents(String urlEvents, Class<EventsResponse> responseType) {
+        log.info("Fetching from KudaGo API...");
+        EventsResponse eventResponse = restTemplate.getForObject(urlEvents, responseType);
+
+        if (eventResponse != null && eventResponse.getResults() != null) {
+            log.info("Events fetched: {}", eventResponse.getResults().size());
+            return eventResponse.getResults();
+        }
+        return Collections.emptyList();
+    }
+}
+
